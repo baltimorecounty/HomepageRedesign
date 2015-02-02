96bd418287720a6d3afbaa99c6d9de3cdf341933
Marty Powell <jpowel25@gmail.com>
2015-02-02 15:17:06 -0500

    Added newsfeed code
    Shows a news summary on page load

--- assets/js/template.js
+++ assets/js/template.js
@@ -1,9 +1,26 @@
-(function($){
-  // Safe, scoped JavaScript here. Phew.
-})(jQuery);
+(function ($) {
+    var showNewsSummary = function () {
+        var $newsItems = $('.item');
 
-$(document).ready(function(){
-	$('.carousel').slick({
-		dots: true
-	});
-});
+        $newsItems.each(function ($item) {
+            var $newsItem = $(this),
+                $newsItemDesc = $newsItem.find('.description').find('seml');
+
+            $('p:not(:nth-child(2))', $newsItemDesc).hide();
+
+        });
+    };
+
+
+    $(document).ready(function() {
+    	//Hide the neccessary items to show the news summary
+    	//Used because Site Executive does not offer this feature
+	    showNewsSummary();
+
+	    //Initialize the Slick Carousel for County Promotions
+	    $('.carousel').slick({
+	        dots: true
+	    });
+    });
+    
+})(jQuery);
