b8eb6bf828b069bdde4f653e26dd7da9fee4fd86
Marty Powell <jpowel25@gmail.com>
2015-02-03 14:28:39 -0500

    Added flickr feed js

--- assets/js/template.js
+++ assets/js/template.js
@@ -1,4 +1,4 @@
-(function ($) {
+(function ($, Handlebars) {
     /*News Related Methods*/
     var createReadMoreLink = function ($newsItem, $link) {
         $newsItem.append($link.clone().html("Read More"));
@@ -52,10 +52,35 @@
         });
     };
 
+    /*Flick Photo Feeed*/
+    var flickrTemplate = "{{#each this}}<img src='http://farm{{farm}}.static.flickr.com/{{server}}/{{id}}_{{secret}}_q.jpg' />{{/each}}",
+        flickrUrl = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20flickr.photosets.photos%20where%20api_key%3D%22ee58ce6536e4b39a95ebdf000ae4adf3%22%20and%20photoset_id%3D%2272157649564338091%22%20limit%204&format=json&diagnostics=true&callback=";
+
+    var loadFlickrFeed = function(url, $container) {
+            $.getJSON(flickrUrl, function(data) {
+              showFlickrFeed(data.query.results.photo, $container);
+            });
+        },
+        showFlickrFeed = function(data, $container) {
+            console.log(flickrTemplate)
+            var htmlTemplate = Handlebars.compile(flickrTemplate);
+            var html = htmlTemplate(data);
+            console.log(data, htmlTemplate, html);
+            //Append the generated html to the container
+            $container.html(html);
+        };
+
+
     $(document).ready(function () {
+        var $flickFeedContainer = $('.county-photo-feed');
+
         //Hide the neccessary items to show the news summary
         //Used because Site Executive does not offer this feature
         showNewsSummaries();
+
+        //Load the flickr feed into the Baltimore County Now Section of the page
+        //above the news
+        loadFlickrFeed(flickrUrl, $flickFeedContainer);
 
         //Initialize the Slick Carousel for County Promotions
         $('.carousel').slick({
@@ -63,4 +88,4 @@
         });
     });
 
-})(jQuery);
+})(jQuery, Handlebars);
