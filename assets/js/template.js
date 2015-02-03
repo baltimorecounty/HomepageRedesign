29c79f8f5d008a30f55aa8871edd4a5f043b2c88
Marty Powell <jpowel25@gmail.com>
2015-02-03 11:58:52 -0500

    Refactored news story javascript
    Also updated headline date format

--- assets/js/template.js
+++ assets/js/template.js
@@ -1,26 +1,66 @@
 (function ($) {
-    var showNewsSummary = function () {
+    /*News Related Methods*/
+    var createReadMoreLink = function ($newsItem, $link) {
+        $newsItem.append($link.clone().html("Read More"));
+    },
+    getDisplayDate = function (date) {
+        var Months = {
+            "Jan": "January",
+                "Feb": "February",
+                "Mar": "March",
+                "Apr": "April",
+                "May": "May",
+                "Jun": "June",
+                "Jul": "July",
+                "Aug": "August",
+                "Sep": "September",
+                "Oct": "October",
+                "Nov": "November",
+                "Dec": "December"
+        },
+        dateParts = date.split(" "),
+            month = Months[dateParts[2]],
+            day = dateParts[1],
+            year = dateParts[3];
+
+        return month + " " + day + ", " + year;
+    },
+    setDisplayDate = function ($newsItem, date) {
+        $('.title', $newsItem).after("<span class='pub-date'>" + date + "</span>");
+    },
+    showNewsSummary = function ($newsItemDesc) {
+        $('p:not(:nth-child(2))', $newsItemDesc).hide();
+    },
+    showNewsSummaries = function () {
         var $newsItems = $('.item');
 
         $newsItems.each(function ($item) {
             var $newsItem = $(this),
-                $newsItemDesc = $newsItem.find('.description').find('seml');
+                $newsItemDesc = $newsItem.find('.description').find('seml'),
+                $linkToEntry = $newsItem.find("h3").find("a"),
+                pubDate = getDisplayDate($('.pubDate', $newsItem).text());
 
-            $('p:not(:nth-child(2))', $newsItemDesc).hide();
+            //Create a read more linked based on the Blog Title's Anchor Href
+            createReadMoreLink($newsItem, $linkToEntry);
+
+            //Include the published date after the news title
+            setDisplayDate($newsItem, pubDate);
+
+            //Remove the body of the blog entry, only show the Summary
+            showNewsSummary($newsItemDesc);
 
         });
     };
 
+    $(document).ready(function () {
+        //Hide the neccessary items to show the news summary
+        //Used because Site Executive does not offer this feature
+        showNewsSummaries();
 
-    $(document).ready(function() {
-    	//Hide the neccessary items to show the news summary
-    	//Used because Site Executive does not offer this feature
-	    showNewsSummary();
-
-	    //Initialize the Slick Carousel for County Promotions
-	    $('.carousel').slick({
-	        dots: true
-	    });
+        //Initialize the Slick Carousel for County Promotions
+        $('.carousel').slick({
+            dots: true
+        });
     });
-    
+
 })(jQuery);
