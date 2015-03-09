9a7e695b822c4b68bdf1feecf825f26779322980
Marty Powell <jpowel25@gmail.com>
2015-03-09 12:43:14 -0400

    Updated flickr feed to use tags

--- assets/js/template.js
+++ assets/js/template.js
@@ -1,4 +1,4 @@
-(function ($, Handlebars, TextResizer) {
+(function ($, Handlebars, TextResizer, Flickr) {
     /*News Related Methods*/
     var createReadMoreLink = function ($newsItem, $link) {
         $newsItem.append($link.clone().html("Read More >>"));
@@ -51,28 +51,7 @@
 
         });
     };
-
-    /*Flick Photo Feeed*/
-    var apiKey = "22ee58ce6536e4b39a95ebdf000ae4adf3",
-        photoSetId = "72157649564338091", 
-        flickrTemplate = "{{#each this}}<div class='county-photo-container col-md-3 col-sm-3 hidden-xs'><a href='https://www.flickr.com/photos/baltimorecounty/{{id}}/in/set-" + photoSetId + "' title='View this photo on Baltimore County&apos;s Flickr Album'><img alt='{{title}}' class='county-photo-feed-item' src='http://farm{{farm}}.static.flickr.com/{{server}}/{{id}}_{{secret}}_q.jpg' alt='{{title}}' /></a></div>{{/each}}",
-        flickrUrl = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20flickr.photosets.photos%20where%20api_key%3D%" + apiKey + "%22%20and%20photoset_id%3D%22" + photoSetId + "%22%20limit%204&format=json&diagnostics=true&callback=";
-
-    var loadFlickrFeed = function (url, $container) {
-        $.getJSON(flickrUrl, function (data) {
-            console.log(data);
-            showFlickrFeed(data.query.results.photo, $container);
-        });
-    },
-    showFlickrFeed = function (data, $container) {
-        var htmlTemplate = Handlebars.compile(flickrTemplate),
-            html = htmlTemplate(data);
-            console.log(html);
-
-        //Append the generated html to the container
-        $container.html(html);
-    };
-
+    
 
     $(document).ready(function () {
         var $flickFeedContainer = $('.county-photo-feed');
@@ -83,7 +62,15 @@
 
         //Load the flickr feed into the Baltimore County Now Section of the page
         //above the news
-        loadFlickrFeed(flickrUrl, $flickFeedContainer);
+        var photoFeed = new Flickr({
+            apiKey: "ee58ce6536e4b39a95ebdf000ae4adf3",
+            nsid: "56007743@N08",
+            searchTags: "bonefish", //comma seperated list
+            template: "{{#each this}}<div class='county-photo-container col-md-3 col-sm-3 hidden-xs'><a href='https://www.flickr.com/photos/baltimorecounty/{{id}}/' title='View this photo on Baltimore County&apos;s Flickr Album'><img alt='{{title}}' class='county-photo-feed-item' src='http://farm{{farm}}.static.flickr.com/{{server}}/{{id}}_{{secret}}_q.jpg' alt='{{title}}' /></a></div>{{/each}}",
+            $container: $flickFeedContainer
+        });
+
+        //loadFlickrFeed(flickrUrl, $flickFeedContainer);
 
         //Initialize the Slick Carousel for County Promotions
         $('.promotions').slick({
@@ -108,4 +95,4 @@
         $('.primary-nav, .secondary-nav').toggleClass('mobile-menu-visible');
     });
 
-})(jQuery, Handlebars, TextResizer);
+})(jQuery, Handlebars, TextResizer, Flickr);
