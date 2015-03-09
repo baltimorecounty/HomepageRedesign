2b6109acded6a23c1bc2b9318a31f0f05f2223a8
Marty Powell <jpowel25@gmail.com>
2015-03-09 10:25:48 -0400

    Added links to flickr images
    Links will navigate the user to full size flickr page.

--- assets/js/template.js
+++ assets/js/template.js
@@ -53,17 +53,21 @@
     };
 
     /*Flick Photo Feeed*/
-    var flickrTemplate = "{{#each this}}<div class='county-photo-container col-md-3 col-sm-3 hidden-xs'><img alt='{{title}}' class='county-photo-feed-item' src='http://farm{{farm}}.static.flickr.com/{{server}}/{{id}}_{{secret}}_q.jpg' /></div>{{/each}}",
-        flickrUrl = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20flickr.photosets.photos%20where%20api_key%3D%22ee58ce6536e4b39a95ebdf000ae4adf3%22%20and%20photoset_id%3D%2272157649564338091%22%20limit%204&format=json&diagnostics=true&callback=";
+    var apiKey = "22ee58ce6536e4b39a95ebdf000ae4adf3",
+        photoSetId = "72157649564338091", 
+        flickrTemplate = "{{#each this}}<div class='county-photo-container col-md-3 col-sm-3 hidden-xs'><a href='https://www.flickr.com/photos/baltimorecounty/{{id}}/in/set-" + photoSetId + "' title='View this photo on Baltimore County&apos;s Flickr Album'><img alt='{{title}}' class='county-photo-feed-item' src='http://farm{{farm}}.static.flickr.com/{{server}}/{{id}}_{{secret}}_q.jpg' alt='{{title}}' /></a></div>{{/each}}",
+        flickrUrl = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20flickr.photosets.photos%20where%20api_key%3D%" + apiKey + "%22%20and%20photoset_id%3D%22" + photoSetId + "%22%20limit%204&format=json&diagnostics=true&callback=";
 
     var loadFlickrFeed = function (url, $container) {
         $.getJSON(flickrUrl, function (data) {
+            console.log(data);
             showFlickrFeed(data.query.results.photo, $container);
         });
     },
     showFlickrFeed = function (data, $container) {
         var htmlTemplate = Handlebars.compile(flickrTemplate),
             html = htmlTemplate(data);
+            console.log(html);
 
         //Append the generated html to the container
         $container.html(html);
