a2a1908d7eaefe399bf27ec9a9814169c20afd9b
Marty Powell <jpowel25@gmail.com>
2015-03-16 09:50:34 -0400

    Reformatted Code

--- assets/js/flickr.js
+++ assets/js/flickr.js
@@ -24,41 +24,11 @@ var Flickr = (function(window, undefined, $, Handlebars) {
 		        var htmlTemplate = Handlebars.compile(_this.template),
 		            html = htmlTemplate(data);
 
-		            console.log(html);
-
-		        //Append the generated html to the container
-		        //_this.$container.html(html);
-
-		        console.log(_this.$container.html())
-
-		        _this.$container.html("fart");
-
 		        _this.$container.html(html);
 		    };
 
 		    init();
 
 	};
-
-	/*Flick Photo Feeed*/
-    // var apiKey = "ee58ce6536e4b39a95ebdf000ae4adf3",
-    //     photoSetId = "72157649564338091", 
-    //     flickrTemplate = "{{#each this}}<div class='county-photo-container col-md-3 col-sm-3 hidden-xs'><a href='https://www.flickr.com/photos/baltimorecounty/{{id}}/in/set-" + photoSetId + "' title='View this photo on Baltimore County&apos;s Flickr Album'><img alt='{{title}}' class='county-photo-feed-item' src='http://farm{{farm}}.static.flickr.com/{{server}}/{{id}}_{{secret}}_q.jpg' alt='{{title}}' /></a></div>{{/each}}",
-    //     flickrUrl = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20flickr.photosets.search%20where%20api_key%3D%22" + apiKey + "%22%20and%20tags%3D%22" + this.searchTags + "%22%20limit%204&format=json&diagnostics=true&callback=";
-
-    // var loadFlickrFeed = function (url, $container) {
-    //     $.getJSON(flickrUrl, function (data) {
-    //         showFlickrFeed(data.query.results.photo, $container);
-    //     });
-    // },
-    // showFlickrFeed = function (data, $container) {
-    //     var htmlTemplate = Handlebars.compile(flickrTemplate),
-    //         html = htmlTemplate(data);
-    //         console.log(html);
-
-    //     //Append the generated html to the container
-    //     $container.html(html);
-    // };
-
     return Flickr;
 })(window, undefined, jQuery, Handlebars);
