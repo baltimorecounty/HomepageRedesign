86c54c6bd45ae11de80c3a512ae337f36728d375
Marty Powell <jpowel25@gmail.com>
2015-04-10 08:10:57 -0400

    Switching Machines

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -8,20 +8,23 @@
 	        apiKey: "ee58ce6536e4b39a95ebdf000ae4adf3",
 	        nsid: "56007743@N08",
 	        searchTags: "featured", //comma seperated list
-	        template: "{{#each this}}<div class='county-photo-container col-md-3 col-sm-3 hidden-xs'><a href='https://www.flickr.com/photos/baltimorecounty/{{id}}/' title='View this photo on Baltimore County&apos;s Flickr Album'><img alt='{{title}}' class='county-photo-feed-item' src='http://farm{{farm}}.static.flickr.com/{{server}}/{{id}}_{{secret}}_q.jpg' alt='{{title}}' /></a></div>{{/each}}",
+	        template: "{{#each this}}<div class='county-photo-container col-md-3 col-sm-3 hidden-xs'><a href='//www.flickr.com/photos/baltimorecounty/{{id}}/' title='View this photo on Baltimore County&apos;s Flickr Album'><img alt='{{title}}' class='county-photo-feed-item' src='//farm{{farm}}.static.flickr.com/{{server}}/{{id}}_{{secret}}_q.jpg' alt='{{title}}' /></a></div>{{/each}}",
 	        $container: $flickFeedContainer
 	    });
 
 	    //Initialize the Slick Carousel for County Promotions
         $('.carousel').slick({
             autoplay: true,
+            adaptiveHeight: true,
             autoplaySpeed: 5000,
             dots: true,
             /*Show dot navigation*/
-            nextArrow: "<img src='http://baltimorecountymd.gov/sebin/v/i/carousel-arrow-right.png' class='slick-next' />",
+            nextArrow: "<img src='//baltimorecountymd.gov/sebin/v/i/carousel-arrow-right.png' class='slick-next' />",
             /*Starts on slide 4*/
-            prevArrow: "<img src='http://baltimorecountymd.gov/sebin/f/m/carousel-arrow-left.png 'class='slick-prev' />"
+            prevArrow: "<img src='//baltimorecountymd.gov/sebin/f/m/carousel-arrow-left.png 'class='slick-prev' />"
         });
+
+        
 
         /*Initialize teh Text Resizer*/
         var textResizer = new TextResizer({
---
+++ assets/js/ieLT9.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+	$('.slick-next').click();
+	$('.slick-prev').click();
+    $('.carousel').show();
+});
